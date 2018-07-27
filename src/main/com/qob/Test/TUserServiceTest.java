60cd817e81c12ddd3d38908b7812bd4180c65991
qiaoliwei-pc <qiaolw0709@foxmail.com>
2018-07-27 18:32:04 +0800

    some change testng

--- src/main/com/qob/Test/TUserServiceTest.java
+++ src/main/com/qob/Test/TUserServiceTest.java
@@ -1,9 +1,13 @@
 package com.qob.Test;
 
+import com.qob.Pojo.TUser;
 import com.qob.Service.TUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
 
-public class TUserServiceTest {
+import static org.testng.Assert.*;
+
+public class TUserServiceTest{
     private TUserService userService;
 
     @Autowired
@@ -11,9 +15,17 @@ public class TUserServiceTest {
         this.userService = userService;
     }
 
+    @Test
     public void hasMatchUser(){
         boolean b1 = userService.hasMatchUser("admin", "123456");
         boolean b2 = userService.hasMatchUser("admin", "1111");
+        assertTrue(b1);
+        assertTrue(b2);
+    }
 
+    @Test
+    public void findUserByUserName(){
+        TUser user = userService.findUserByUserName("admin");
+        assertEquals(user.getUserName(), "admin");
     }
 }
